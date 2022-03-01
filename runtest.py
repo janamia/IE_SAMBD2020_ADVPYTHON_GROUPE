7ca5494df1d80762770e109514f6dfbf181ae37e
Jana Jindan <janajindan@Janas-MacBook-Pro.local>
2022-03-01 12:45:54 +0300

    first commit

--- runtest.py
+++ runtest.py
@@ -1,4 +1,4 @@
-from pythonpackagesample import datareader_doc
+from IE_SAMBD2020_ADVPYTHON_GROUPE import datareader_doc
 
 print(help(datareader_doc))
 
--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='pythonpackagesample',
+setup(name='IE_SAMBD2020_ADVPYTHON_GROUPE',
       version='0.1',  # Development release
       description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
       url='https://github.com/manoelgadi/pythonpackagesample',
-      author='Prof. Manoel Gadi',
-      author_email='mfalonso@faculty.ie.edu',
+      author='GROUPE',
+      author_email='jana.jindan@gmail.com',
       license='MIT',
-          packages=['pythonpackagesample'],
+          packages=['IE_SAMBD2020_ADVPYTHON_GROUPE'],
       zip_safe=False)
