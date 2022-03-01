0124604560f60ffdb6235f7ae68666e9f3cafcd4
Jana Jindan <janajindan@Janas-MacBook-Pro.local>
2022-03-01 13:35:25 +0300

    first commit

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 setup(name='IE_SAMBD2020_ADVPYTHON_GROUPE',
       version='0.1',  # Development release
       description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
-      url='https://github.com/manoelgadi/pythonpackagesample',
+      #url='https://github.com/manoelgadi/pythonpackagesample',
+      url='https://github.com/janamia/IE_SAMBD2020_ADVPYTHON_GROUPE.git',
       author='GROUPE',
       author_email='jana.jindan@gmail.com',
       license='MIT',
